a7b37cf0444495558c6878b9ad1b1c56a18e6611
Shenchuanhuan <shen_nanping@163.com>
2017-09-05 15:33:16 +0800

    搭首页

--- src/container/home/index.js
+++ src/container/home/index.js
@@ -1,16 +1,18 @@
 import React,{ Component } from 'react';
 import { Link } from 'react-router';
 
+import Head from 'Component/head';
+
 class Home extends Component {
     render() {
         return(
             <div>
+                <Head/>
                 <ul>
                     <li><Link to='/first'>First</Link></li>
                     <li><Link to='/second'>Second</Link></li>
                     <li><Link to='/third'>Third</Link></li>
                 </ul>
-                {this.props.children}
             </div>
         )
     }
